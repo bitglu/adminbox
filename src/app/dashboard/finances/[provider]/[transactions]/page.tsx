9c15b61bd236132eac0a5029efe04597345ff7b3
Bitglu <bitglu7@gmail.com>
2024-04-26 23:48:21 -0400

    Actualizar página de transacciones de finanzas***
    ***Actualizar página de transacciones de finanzas

--- src/app/dashboard/finances/[provider]/[transactions]/page.tsx
+++ src/app/dashboard/finances/[provider]/[transactions]/page.tsx
@@ -690,13 +690,13 @@ export default function Home({ params }: { params: { transactions: string } }) {
               valueStyle={{ color: "#2439C4" }}
               prefix="$"
             />
-            {/* <Statistic
+            <Statistic
               title="Total Charge"
               value={Math.round(chargeAmount * 100) / 100}
               style={{ marginLeft: 10, marginRight: 10 }}
               valueStyle={{ color: "#C9348A" }}
               prefix="$"
-            /> */}
+            />
             <Statistic
               title="Total Checks"
               value={Math.round(chargeCheck * 100) / 100}
@@ -734,9 +734,9 @@ export default function Home({ params }: { params: { transactions: string } }) {
         >
           <Form.Item name="type" label="Type" rules={[{ required: true }]}>
             <Select>
-              <Select.Option value="Credit">Credit Card</Select.Option>
+              <Select.Option value="Credit">Credit</Select.Option>
               <Select.Option value="Cash">Cash</Select.Option>
-              {/* <Select.Option value="Charge">Charge</Select.Option> */}
+              <Select.Option value="Charge">Charge</Select.Option>
               <Select.Option value="Checks">Checks</Select.Option>
             </Select>
           </Form.Item>
